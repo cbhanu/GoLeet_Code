2d6a49981a56ee6b367b3f337b0137313af3c897
Chinmayee.Bhanu <chinmayee.bhanu15@gmail.com>
2018-06-29 16:19:09 -0700

    Removing duplicates from sorted array

---
+++ remove_duplicates_sorted_array.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+
+	for i:=0;i<len(nums)-1;i++{
+		cnt:=0
+		//fmt.Println(nums[i],nums[i+1])
+		if nums[i+1] == nums[i] {
+			cnt+=1
+			for j:=i+1;j<len(nums)-1;j++{
+				if nums[j+1] != nums[i] {
+					break
+				}else{cnt += 1}
+
+		}
+		fmt.Println(nums)
+		nums = append(nums[:i], nums[i+cnt:]...)
+		}
+	}
+	return len(nums)
+}
+
+func main() {
+	val_ip := []int{0,0,0,1,1,1,2,2,3,3,4,5, 6, 7, 7, 8}
+	lenn := removeDuplicates(val_ip)
+	fmt.Println("Final answer: ")
+	for i:= 0; i < lenn; i++{
+		fmt.Print(val_ip[i]);
+	}
+	fmt.Println("\nLength is:",lenn)
+
+}
