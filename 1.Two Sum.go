b8382c582d8a0a4a3a16c70621c48f3f507258d9
Chinmayee.Bhanu <chinmayee.bhanu15@gmail.com>
2018-06-29 14:26:03 -0700

    Problem Two Sum

--- 1.Two Sum.go
+++ 1.Two Sum.go
@@ -1,5 +1,40 @@
 package main
 
+import "fmt"
+
+/* My Brute Force approach*/
+func twoSum(nums []int, target int) []int {
+
+	//var ans []int // slice to store answer in
+	for i := range nums{
+		for j:=i+1;j<len(nums);j++{
+			if nums[i] + nums[j] == target{
+				//return append(ans, i, j)
+				return []int{i,j}
+
+			}
+		}
+
+	}
+	return nil
+}
+/* The following is the best solution in terms of runtime for Go*/
+
+func twoSum_best(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		comp := target - nums[i]
+		if _, ok := m[comp]; ok {
+			return []int {m[comp], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
+
+
 func main() {
-	
+	input:= []int{2,8,7,11}
+	//twoSum_best(input,9)
+	fmt.Printf("Answer : %d", twoSum(input,9))
 }
