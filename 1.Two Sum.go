181d81d5e00039830e8e2073d7f8abcd4957ec43
Chinmayee.Bhanu <chinmayee.bhanu15@gmail.com>
2018-06-29 14:49:12 -0700

    Problem Two Sum with best solution

--- 1.Two Sum.go
+++ 1.Two Sum.go
@@ -1,40 +1,44 @@
+/*
+The earlier easier codes contain some over detailed comments for a comprehensive read
+ */
+
 package main
 
 import "fmt"
 
 /* My Brute Force approach*/
 func twoSum(nums []int, target int) []int {
-
-	//var ans []int // slice to store answer in
-	for i := range nums{
-		for j:=i+1;j<len(nums);j++{
-			if nums[i] + nums[j] == target{
+	//var ans []int
+	for i, first := range nums{
+		for j, second := range nums[1:]{
+			if first + second == target{
 				//return append(ans, i, j)
-				return []int{i,j}
-
+				return []int{i,j+1}
 			}
 		}
 
 	}
 	return nil
 }
+
 /* The following is the best solution in terms of runtime for Go*/
 
 func twoSum_best(nums []int, target int) []int {
 	m := make(map[int]int)
-	for i := 0; i < len(nums); i++ {
-		comp := target - nums[i]
-		if _, ok := m[comp]; ok {
+	for i, curr := range nums{
+		comp := target - curr
+		_, ok := m[comp]
+		if ok {
 			return []int {m[comp], i}
+			// When ok is true, the comparison value, comp is going to be the first number, since curr is the second number
 		}
-		m[nums[i]] = i
+		m[nums[i]] = i // current difference value is the key , index is the dictionary value
 	}
 	return nil
 }
 
-
 func main() {
 	input:= []int{2,8,7,11}
-	//twoSum_best(input,9)
-	fmt.Printf("Answer : %d", twoSum(input,9))
+	//twoSum(input,9)
+	fmt.Printf("\nAnswer : %d", twoSum_best(input,9))
 }
