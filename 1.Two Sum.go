59417361be04ee41d8cd9ad6e40e336f62bd87c7
cbhanu <32722001+cbhanu@users.noreply.github.com>
2018-06-29 14:27:51 -0700

    Update 1.Two Sum.go

--- 1.Two Sum.go
+++ 1.Two Sum.go
@@ -6,9 +6,9 @@ import "fmt"
 func twoSum(nums []int, target int) []int {
 
 	//var ans []int // slice to store answer in
-	for i := range nums{
-		for j:=i+1;j<len(nums);j++{
-			if nums[i] + nums[j] == target{
+	for i, first := range nums{
+		for j:=i+1;j<len(nums);j++{ // for j, second := range nums[1:]
+			if first + nums[j] == target{
 				//return append(ans, i, j)
 				return []int{i,j}
 
